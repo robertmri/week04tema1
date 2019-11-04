5e758da949a7a09ee4dacf0f75a2d0827ab4a611
robertmri <56820682+robertmri@users.noreply.github.com>
2019-11-04 10:39:10 +0200

    Add files via upload

---
+++ Professor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema
+{
+    class Professor
+    {
+        public Professor(string name, string faculty, string specialization)
+        {
+            this.name = name;
+            this.faculty = faculty;
+            this.specialization = specialization;
+        }
+        public string name { get; set; }
+        public string faculty { get; set; }
+        public string specialization { get; set; }
+
+        public string Afisare
+        {
+            get { return $"Profesorul {name} preda la facultatea {faculty} specializarea {specialization}."; }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Manole Robert", "FEAA", 3);
+            Console.WriteLine(student1.Afisare);
+
+            Professor profesor1 = new Professor("Ion Vasile", "Automatica Si Calculatoare", "IT");
+            Console.WriteLine(profesor1.Afisare);
+        }
+    }
+}
